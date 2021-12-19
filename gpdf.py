a6566ab8ac857c86fae63aed9b50116d1be9ee1f
Kenneth Francis <36281899+kencav@users.noreply.github.com>
2021-12-19 06:01:28 +0000

    made this shit work

--- gpdf.py
+++ gpdf.py
@@ -1,45 +1,69 @@
 #! python3
 
-#packages
+#libraries
+import pdfplumber
+import os
+
 import openai
 import wget
 import pathlib
+import pdfplumber
+import numpy as np
 
-#function to download a pdf of the paper
-def getPaper(paper_url, filename=""):
-    """
-    Downloads a paper from it's arxiv page and returns
-    the local path to that file.
-    """
-    downloadedPaper = wget.download(paper_url, filename)    
-    downloadedPaperFilePath = pathlib.Path(downloadedPaper)
 
-    return downloadedPaperFilePath
+#write a function that downloads a pdf from an arxiv address
+def getPaper(paper_url, filename="random_paper.pdf"):
+  """
+  Downloads a paper from it's arxiv page and returns
+  the local path to that file.
+  """
+  downloadedPaper = wget.download(paper_url, filename)    
+  downloadedPaperFilePath = pathlib.Path(downloadedPaper)
 
-#function to convert paper to text
+  return downloadedPaperFilePath
+#download paper
+getPaper(r'https://arxiv.org/pdf/2112.08990.pdf')
+
+#asign file name
+paperFilePath ="random_paper.pdf"
+#extract content with pdf pdfplumber
+def getPaperContent(paperFilePath):
+  return pdfplumber.open(paperFilePath).pages
+
+paperContent = getPaperContent(paperFilePath)
+#func to display content
 def displayPaperContent(paperContent, page_start=0, page_end=5):
     for page in paperContent[page_start:page_end]:
         print(page.extract_text())
+#run func
+#displayPaperContent(paperContent)
 
-#feed gpt3 paper text
-def showPaperSummary(paperContent):
-    #setting the tag so gpt3 knows when the text stops and when to start the completion
-    tldr_tag = "\n\n tl;dr:" 
-    #set up environment
-    openai.organization = 'org-cur38Rg6pd6JFV2pDFNAFtEh'
-    openai.api_key = "sk-JXm3aEq3AAUezUoKjGD2qCJ46xf5aAEUmMmUsbwf"
-    # calling the engines available from the openai api 
+#open ai API
+my_secret = os.environ['API_KEY']
+
+#summarize paper
+def getPaperSummary(paperContent):
+    tldr_tag = "\n tl;dr:" #writing the tag so that the GPT-3 model knows when the text stops and when it should start the completion
+    openai.api_key = my_secret
     engine_list = openai.Engine.list()
-
-    #extract the text from each page
+    
     for page in paperContent:    
-        text = page.extract_text() + tldr_tag
-        #feeding the text to the model
-        response = openai.Completion.create(engine="curie",prompt=text,temperature=0.3,
-            max_tokens=60,
+        text = page.extract_text() + tldr_tag #xtracting the text from each page and feeding it to the model
+        response = openai.Completion.create(engine="davinci",prompt=text,temperature=0.3,
+            max_tokens=240,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
             stop=["\n"]
         )
-        print(response["choices"][0]["text"])
+        summary = response["choices"][0]["text"]
+
+    return summary
+
+# paperSummary = getPaperSummary(paperContent)
+
+# print('...')
+# print('...')
+# print('...')
+# print()
+# print(paperSummary)
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-#! python3
+import tkinter as tk
+from tkinter import simpledialog
+import gpdf
 
-#libraries
-from gpdf import showPaperSummary
-import pdfplumber
-import os
+####
 
-#show the summary
-#convert pdf to text
-paperFilePath = "hrmj3.pdf"
-paperContent = pdfplumber.open(paperFilePath).pages
-showPaperSummary(paperContent)
+#summary
+
+####
+#download paper
 
 
+# gpdf.getPaper(r'https://arxiv.org/pdf/2112.08990.pdf', filename="random_paper.pdf")
 
+#asign file name
+paperFilePath ="random_paper.pdf"
+#extract content with pdf pdfplumber
+paperContent = gpdf.getPaperContent(paperFilePath)
+
+paperSummary = gpdf.getPaperSummary(paperContent)
+
+print('...')
+print('...')
+print('...')
+print()
+print(paperSummary)
