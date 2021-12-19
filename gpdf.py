072a83c195ecf815e436c05fab09bff87260da8e
Kenneth Francis <36281899+kencav@users.noreply.github.com>
2021-12-19 07:32:19 +0000

    final baseline version that takes a paper file or arxiv link

--- gpdf.py
+++ gpdf.py
@@ -1,69 +1,69 @@
-#! python3
+# #! python3
 
-#libraries
-import pdfplumber
-import os
+# #libraries
+# import pdfplumber
+# import os
 
-import openai
-import wget
-import pathlib
-import pdfplumber
-import numpy as np
+# import openai
+# import wget
+# import pathlib
+# import pdfplumber
+# import numpy as np
 
 
-#write a function that downloads a pdf from an arxiv address
-def getPaper(paper_url, filename="random_paper.pdf"):
-  """
-  Downloads a paper from it's arxiv page and returns
-  the local path to that file.
-  """
-  downloadedPaper = wget.download(paper_url, filename)    
-  downloadedPaperFilePath = pathlib.Path(downloadedPaper)
+# #write a function that downloads a pdf from an arxiv address
+# def getPaper(paper_url, filename="random_paper.pdf"):
+#   """
+#   Downloads a paper from it's arxiv page and returns
+#   the local path to that file.
+#   """
+#   downloadedPaper = wget.download(paper_url, filename)    
+#   downloadedPaperFilePath = pathlib.Path(downloadedPaper)
 
-  return downloadedPaperFilePath
-#download paper
-#getPaper(r'https://arxiv.org/pdf/2112.08990.pdf')
+#   return downloadedPaperFilePath
+# #download paper
+# #getPaper(r'https://arxiv.org/pdf/2112.08990.pdf')
 
-#asign file name
-paperFilePath ="random_paper.pdf"
-#extract content with pdf pdfplumber
-def getPaperContent(paperFilePath):
-  return pdfplumber.open(paperFilePath).pages
+# #asign file name
+# paperFilePath ="random_paper.pdf"
+# #extract content with pdf pdfplumber
+# def getPaperContent(paperFilePath):
+#   return pdfplumber.open(paperFilePath).pages
 
-paperContent = getPaperContent(paperFilePath)
-#func to display content
-def displayPaperContent(paperContent, page_start=0, page_end=5):
-    for page in paperContent[page_start:page_end]:
-        print(page.extract_text())
-#run func
-#displayPaperContent(paperContent)
+# paperContent = getPaperContent(paperFilePath)
+# #func to display content
+# def displayPaperContent(paperContent, page_start=0, page_end=5):
+#     for page in paperContent[page_start:page_end]:
+#         print(page.extract_text())
+# #run func
+# #displayPaperContent(paperContent)
 
-#open ai API
-my_secret = os.environ['API_KEY']
+# #open ai API
+# my_secret = os.environ['API_KEY']
 
-#summarize paper
-def getPaperSummary(paperContent):
-    tldr_tag = "\n tl;dr:" #writing the tag so that the GPT-3 model knows when the text stops and when it should start the completion
-    openai.api_key = my_secret
-    #engine_list = openai.Engine.list()
+# #summarize paper
+# def getPaperSummary(paperContent):
+#     tldr_tag = "\n tl;dr:" #writing the tag so that the GPT-3 model knows when the text stops and when it should start the completion
+#     openai.api_key = my_secret
+#     #engine_list = openai.Engine.list()
     
-    for page in paperContent:    
-        text = page.extract_text() + tldr_tag #xtracting the text from each page and feeding it to the model
-        response = openai.Completion.create(engine="davinci",prompt=text,temperature=0.3,
-            max_tokens=240,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=["\n"]
-        )
-        summary = response["choices"][0]["text"]
+#     for page in paperContent:    
+#         text = page.extract_text() + tldr_tag #xtracting the text from each page and feeding it to the model
+#         response = openai.Completion.create(engine="davinci",prompt=text,temperature=0.3,
+#             max_tokens=240,
+#             top_p=1,
+#             frequency_penalty=0,
+#             presence_penalty=0,
+#             stop=["\n"]
+#         )
+#         summary = response["choices"][0]["text"]
 
-    return summary
+#     return summary
 
-paperSummary = getPaperSummary(paperContent)
+# paperSummary = getPaperSummary(paperContent)
 
-print('...')
-print('...')
-print('...')
-print()
-print('Summary:',paperSummary)
+# print('...')
+# print('...')
+# print('...')
+# print()
+# print('Summary:',paperSummary)
--- main.py
+++ main.py
@@ -1,26 +1,69 @@
-import tkinter as tk
-from tkinter import simpledialog
-import gpdf
+#! python3
 
-####
+#libraries
+import os
+import openai
+import wget
+import pathlib
+import pdfplumber
 
-#summary
+#write a function that downloads a pdf from an arxiv address
+def getPaper(paper_url, filename="random_paper.pdf"):
+  """
+  Downloads a paper from it's arxiv page and returns
+  the local path to that file.
+  """
+  downloadedPaper = wget.download(paper_url, filename)    
+  downloadedPaperFilePath = pathlib.Path(downloadedPaper)
 
-####
+  return downloadedPaperFilePath
+
+#asign file name
+
+paperFilePath ="ona.pdf"
+paperURL = r'https://arxiv.org/pdf/2112.08990.pdf'
+
 #download paper
+#getPaper(paperURL, paperFilePath)
 
+#extract content with pdf pdfplumber
+def getPaperContent(paperFilePath):
+  return pdfplumber.open(paperFilePath).pages
 
-# # gpdf.getPaper(r'https://arxiv.org/pdf/2112.08990.pdf', filename="random_paper.pdf")
+paperContent = getPaperContent(paperFilePath)
+#func to display content
+def displayPaperContent(paperContent, page_start=0, page_end=5):
+    for page in paperContent[page_start:page_end]:
+        print(page.extract_text())
+#run func
+displayPaperContent(paperContent)
 
-# #asign file name
-# paperFilePath ="random_paper.pdf"
-# #extract content with pdf pdfplumber
-# paperContent = gpdf.getPaperContent(paperFilePath)
+#open ai API
+my_secret = os.environ['API_KEY']
 
-# paperSummary = gpdf.getPaperSummary(paperContent)
+#summarize paper
+def getPaperSummary(paperContent):
+    tldr_tag = "\n tl;dr:" #writing the tag so that the GPT-3 model knows when the text stops and when it should start the completion
+    openai.api_key = my_secret
+    #engine_list = openai.Engine.list()
+    
+    for page in paperContent:    
+        text = page.extract_text() + tldr_tag #xtracting the text from each page and feeding it to the model
+        response = openai.Completion.create(engine="davinci",prompt=text,temperature=0.3,
+            max_tokens=140,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n"]
+        )
+        summary = response["choices"][0]["text"]
 
-# print('...')
-# print('...')
-# print('...')
-# print()
-# print(paperSummary)
+    return summary
+
+paperSummary = getPaperSummary(paperContent)
+
+print('...')
+print('...')
+print('...')
+print()
+print('Summary:',paperSummary)
